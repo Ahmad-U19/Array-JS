0c6ad8e736cb4d9f836b75d3693e81eb2245babb
Ahmad-U19 <ahmedoppal2592@gmail.com>
2024-11-25 22:08:48 +0500

    Added JS File

---
+++ script.js
@@ -0,0 +1,17 @@
+var arr=[1,"Ahmad Uppal",false,4,5];
+
+for(var i=0;i<5;i++){
+    document.write(arr[i]+"<br>")
+}
+
+var g=new Array(4);
+
+document.write("<br>")
+
+for(var i=0;i<4;i++){
+    g[i]=prompt("Enter the Value");
+}
+
+for(var i=0;i<4;i++){
+    document.write(g[i]+"<br>")
+}
